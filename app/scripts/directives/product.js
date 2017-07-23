350385841ac7ed57e70a421d353d196659431282
Syed imty <syed.imty@gmail.com>
2017-07-24 05:05:26 +0800

    Basic styling

--- app/scripts/directives/product.js
+++ app/scripts/directives/product.js
@@ -7,40 +7,46 @@
  * # product
  */
 angular.module('sampleAngularApp')
-  .directive('product', function ($http,$location, remoteCall) {
-    return {
-      templateUrl: 'views/product.html',
-      restrict: 'E',
-       scope: {
-	      model: '=',
-	      navigate:"="
-	   },
-      link: function postLink(scope, element, attrs) {
-				//scope.model = attrs.name;
-				scope.minimumBid = scope.model.lastBid ? scope.model.lastBid.price + scope.model.inc : scope.model.rPrice;
-        scope.bidObject = {
-						user : scope.model.user || "user",
-						price : scope.minimumBid
-				};
-				scope.addBid = function(){
-						remoteCall.put('products/' + this.model.name + '/bid',{
-									user : this.model.user || "syed",
-									price : this.bidObject.price
-						}).then(response => {
-									this.data = response.data;
-						});
-				}
-				scope.validataBid = function () {
-						var currentBid = scope.bidObject.price, minimumBid = scope.minimumBid
-						if(!currentBid || currentBid < minimumBid) {
-								scope.error = "Invalid bid amout"
-						} else {
-								scope.error = null;
-						}
-				}
-				scope.navigateToBidPage = function(){
-					$location.path('/products/'+ this.model.name);
-				}
-	   }
-    };
-  });
+    .directive('product', function($http, $location, remoteCall) {
+        return {
+            templateUrl: 'views/product.html',
+            restrict: 'E',
+            scope: {
+                model: '=',
+                navigate: "=",
+
+            },
+            link: function postLink(scope, element, attrs) {
+                //scope.model = attrs.name;
+                scope.updateBidObject = function() {
+                    scope.minimumBid = scope.model.lastBid ? scope.model.lastBid.price + scope.model.inc : scope.model.rPrice;
+                    scope.bidObject = {
+                        user: scope.model.user || "user",
+                        price: scope.minimumBid
+                    };
+                };
+                scope.updateBidObject();
+                scope.addBid = function() {
+                    remoteCall.put('products/' + this.model.name + '/bid', {
+                        user: this.model.user || "syed",
+                        price: this.bidObject.price
+                    }).then(response => {
+                        scope.model.lastBid = response.data.lastBid;
+                        this.updateBidObject(scope);
+                    });
+                };
+                scope.validataBid = function() {
+                    var currentBid = scope.bidObject.price,
+                        minimumBid = scope.minimumBid
+                    if (!currentBid || currentBid < minimumBid) {
+                        scope.error = "Invalid bid amout"
+                    } else {
+                        scope.error = null;
+                    }
+                };
+                scope.navigateToBidPage = function() {
+                    $location.path('/products/' + this.model.name);
+                }
+            }
+        };
+    });

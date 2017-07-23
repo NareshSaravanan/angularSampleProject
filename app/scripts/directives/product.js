b5867edd87ad69b6d43bf8c06f87854ed8a6a56c
Syed imty <syedimdiyasa@virtusa.com>
2017-07-24 02:30:49 +0800

    Validation

--- app/scripts/directives/product.js
+++ app/scripts/directives/product.js
@@ -7,7 +7,7 @@
  * # product
  */
 angular.module('sampleAngularApp')
-  .directive('product', function ($http,$location) {
+  .directive('product', function ($http,$location, remoteCall) {
     return {
       templateUrl: 'views/product.html',
       restrict: 'E',
@@ -16,19 +16,31 @@ angular.module('sampleAngularApp')
 	      navigate:"="
 	   },
       link: function postLink(scope, element, attrs) {
-        //scope.model = attrs.name;
-        scope.bidObject = {};
-		scope.addBid = function(){
-		    $http.put('http://localhost:5000/api/products/'+this.model.name+'/bid',{
-		      user : this.model.user || "syed",
-		      price : this.bidObject.price
-		    }).then(response => {
-		          this.data = response.data;
-		        });
-		}
-		scope.navigateToBidPage = function(){
-		   $location.path('/products/'+ this.model.name);
-		}
+				//scope.model = attrs.name;
+				scope.minimumBid = scope.model.lastBid ? scope.model.lastBid.price + scope.model.inc : scope.model.rPrice;
+        scope.bidObject = {
+						user : scope.model.user || "user",
+						price : scope.minimumBid
+				};
+				scope.addBid = function(){
+						remoteCall.put('products/' + this.model.name + '/bid',{
+									user : this.model.user || "syed",
+									price : this.bidObject.price
+						}).then(response => {
+									this.data = response.data;
+						});
+				}
+				scope.validataBid = function () {
+						var currentBid = scope.bidObject.price, minimumBid = scope.minimumBid
+						if(!currentBid || currentBid < minimumBid) {
+								scope.error = "Invalid bid amout"
+						} else {
+								scope.error = null;
+						}
+				}
+				scope.navigateToBidPage = function(){
+					$location.path('/products/'+ this.model.name);
+				}
 	   }
     };
   });
